e2f3aed82d17e0815ea1e2fa31ec9a2a86070e49
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-23 08:37:47 -0300

    Keyword component styles

--- src/components/Keyword.jsx
+++ src/components/Keyword.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import removeIcon from "../assets/icon-remove.svg";
 
 export default function Keyword({ parent, text }) {
     return (
-        <div className="">
+        <div className={`h-8 bg-grayish-cyan-2 ${parent === "job" && "hover:bg-cyan"} rounded-md font-bold flex items-center transition`}>
             {parent === "filter" ? (
-                <div className="">
-                    {text}
-                    <span className="">X</span>
+                <div className="h-full flex items-center rounded-md">
+                    <div className="px-[10px] text-cyan">{text}</div>
+                    <span className="w-8 h-full bg-cyan hover:bg-grayish-cyan-4 text-white flex justify-center items-center rounded-r-md cursor-pointer transition">
+                        <img className="h-[13px]" src={removeIcon} alt="remove icon" />
+                    </span>
                 </div>
-            ) : parent === "job" ? text : null}
+            ) : parent === "job" ? (
+                <div className="px-[10px] text-cyan hover:text-white cursor-pointer">{text}</div>
+            ) : null}
         </div>
     );
 }
