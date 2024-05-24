d7a0aac3436fd5992feee5213b31fe915eb26be9
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-24 09:40:37 -0300

    fetching data from data.json file (not working)

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,21 +5,32 @@ import Job from "./Job";
 
 export default function Content() {
     const [filterKeywords, setFilterKeywords] = useState(['html', 'css', 'javascript']);
-    const [jobsData, setJobsData] = useState(null);
+    const [jobs, setJobs] = useState(null);
 
-    useEffect(() => {
-        // fetch('./data.json')
-        //     .then(res => res.json())
-        //     .then(json => {
-        //         setJobsData(json);
-        //     });
-    }, []);
+    const jobComponents = [];
+
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         try {
+    //             const response = await fetch('./data.json');
+    //             const json = await response.json();
+    //             setJobs(json);
+    //             jobs.map(job => {
+    //                 jobComponents.push(<Job data={job} />)
+    //             })
+    //             console.log(jobComponents)
+    //         } catch(err) {
+    //             console.log(err)
+    //         }
+    //     }
+
+    //     fetchData();
+    // }, []);
 
     return (
         <div className="w-full min-h-[calc(100vh-155px)] flex flex-col items-center bg-grayish-cyan-1 px-6">
             <Filter keywords={filterKeywords} />
-            <Job keywords={['frontend', 'react']} />
-            <Job keywords={['html', 'css', 'javascript', 'python']} />
+            {/* {console.log(jobs)} */}
         </div>
     );
 }
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -3,26 +3,27 @@ import Image from "./Image";
 import Tag from "./Tag";
 import Keyword from "./Keyword";
 
-export default function Job({ tags = {new: true, featured: true}, src, keywords }) {
+export default function Job({ data }) {
+    let keywords = [...data.languages, ...data.tools, data.role, data.level];
     return (
-        <div className={`w-full max-w-[1110px] h-[152px] bg-white rounded-md flex justify-between items-center gap-4 px-10 border-l-[5px] border-solid ${tags.featured ? 'border-cyan' : 'border-transparent'} mb-10 shadow-custom`}>
+        <div key={data.id} className={`w-full max-w-[1110px] h-[152px] bg-white rounded-md flex justify-between items-center gap-4 px-10 border-l-[5px] border-solid ${data.featured ? 'border-cyan' : 'border-transparent'} mb-10 shadow-custom`}>
             <div className="flex items-center gap-6">
-                <Image src="" />
+                <Image src={data.logo} />
                 <div className="flex flex-col gap-2">
                     <div className="flex items-center">
-                        <div className="text-cyan font-bold mr-4 text-lg">Photosnap</div>
-                        {tags.new && <Tag text="new!" />}
-                        {tags.featured && <Tag text="featured" />}
+                        <div className="text-cyan font-bold mr-4 text-lg">{data.company}</div>
+                        {data.new && <Tag text="new!" />}
+                        {data.featured && <Tag text="featured" />}
                     </div>
                     <div className="flex items-center text-[20px] font-bold text-grayish-cyan-4 hover:text-cyan cursor-pointer transition">
-                        Senior Frontend Developer
+                        {data.position}
                     </div>
                     <div className="flex items-center gap-4 text-grayish-cyan-3 text-lg">
-                        <div className="">1d ago</div>
+                        <div className="">{data.postedAt}</div>
                         <span className="w-1 h-1 rounded-full bg-grayish-cyan-3"></span>
-                        <div className="">Full Time</div>
+                        <div className="">{data.contract}</div>
                         <span className="w-1 h-1 rounded-full bg-grayish-cyan-3"></span>
-                        <div className="">USA only</div>
+                        <div className="">{data.location}</div>
                     </div>
                 </div>
             </div>
