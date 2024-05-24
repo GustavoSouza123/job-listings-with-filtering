4b4df84ec9d84556d4f056d66961d7d8c074e8a7
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-24 13:15:59 -0300

    fetching data from data.json file asynchronously (working)

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,32 +5,26 @@ import Job from "./Job";
 
 export default function Content() {
     const [filterKeywords, setFilterKeywords] = useState(['html', 'css', 'javascript']);
-    const [jobs, setJobs] = useState(null);
+    const [jobs, setJobs] = useState([]);
 
-    const jobComponents = [];
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('./data.json');
+                const json = await response.json();
+                setJobs(json.map(job => <Job key={job.id} data={job} />));
+            } catch(err) {
+                console.log(err);
+            }
+        }
 
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         try {
-    //             const response = await fetch('./data.json');
-    //             const json = await response.json();
-    //             setJobs(json);
-    //             jobs.map(job => {
-    //                 jobComponents.push(<Job data={job} />)
-    //             })
-    //             console.log(jobComponents)
-    //         } catch(err) {
-    //             console.log(err)
-    //         }
-    //     }
-
-    //     fetchData();
-    // }, []);
+        fetchData();
+    }, []);
 
     return (
         <div className="w-full min-h-[calc(100vh-155px)] flex flex-col items-center bg-grayish-cyan-1 px-6">
             <Filter keywords={filterKeywords} />
-            {/* {console.log(jobs)} */}
+            {jobs}
         </div>
     );
 }
