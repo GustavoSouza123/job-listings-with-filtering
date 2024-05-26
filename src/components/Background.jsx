e1be9187b62912c36688b3c3eb83067b66d67d2b
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-26 10:52:50 -0300

    refactor background image for the Background component

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import bgHeader from "../assets/bg-header-desktop.svg";
 
 export default function Background() {
+    const style = {
+        backgroundImage: `url(${bgHeader})`,
+        backgroundPosition: 'center',
+    };
+
     return (
-        <div className="w-full h-[155px] bg-cyan bg-desktop-header bg-center"></div>
+        <div className="w-full h-[155px] bg-cyan" style={style}></div>
     );
 }
+
+// tailwind classes for background image: bg-desktop-header bg-center
